780ddf7c48b1ef7dcebf6ff91d4d586efd677046
Ren <renato.regalado@gmail.com>
2018-07-19 10:45:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from functools import wraps
 app = Flask(__name__)
 
 # Config MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'eripsa141'
+app.config['MYSQL_HOST'] = 'hostname'
+app.config['MYSQL_USER'] = 'username'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'plantapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #init MySQL
